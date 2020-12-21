c0faae573e41404c898b26b417e0c450669b1c72
DesistDaydream <373406000@qq.com>
2020-12-21 16:00:40 +0800

    Update interface.go

--- 7.Structs_Interfaces/interface/interface.go
+++ 7.Structs_Interfaces/interface/interface.go
@@ -5,16 +5,20 @@ import (
 	"reflect"
 )
 
-// 定义了一个名为VowelsFinder的接口
+// VowelsFinder 定义了一个名为VowelsFinder的接口
 type VowelsFinder interface {
 	FindVowels() []rune
 }
 
 // 定义了一个基础类型为字符串的名为MyString的类型
-type MyString string
-type MyString1 string
+type (
+	// MyString is
+	MyString string
+	// MyString1 is
+	MyString1 string
+)
 
-//MyString实现了接口VowelsFinder，因为MyString实现了FindVowels方法，且VowelsFinder接口中的所有方法该类型都实现
+// FindVowels MyString实现了接口VowelsFinder，因为MyString实现了FindVowels方法，且VowelsFinder接口中的所有方法该类型都实现
 func (ms MyString) FindVowels() []rune {
 	var vowels []rune
 	for _, rune := range ms {
@@ -25,6 +29,7 @@ func (ms MyString) FindVowels() []rune {
 	return vowels
 }
 
+// FindVowels is
 func (ms MyString1) FindVowels() []rune {
 	var vowels []rune
 	for _, rune := range ms {
@@ -40,15 +45,14 @@ func main() {
 	name1 := MyString1("Tom Anderson")
 	var v VowelsFinder
 	// 接口已声明未使用的时候，接口变量的类型为nil(空),值也为nil(空)，接口的类型会随着值的不同而又不同的类型
-	fmt.Println(reflect.TypeOf(name),reflect.TypeOf(v),v)
-	fmt.Println(reflect.TypeOf(name1),reflect.TypeOf(v),v)
+	fmt.Println(reflect.TypeOf(name), reflect.TypeOf(v), v)
+	fmt.Println(reflect.TypeOf(name1), reflect.TypeOf(v), v)
 	// 由于MyString类型实现了VowelsFinder接口,所以该类型的变量与接口的变量可以互相赋值
 	// 且接口变量的类型变为实现该接口的自定义类型
 	v = name
-	fmt.Printf("自定义类型为:%v,接口类型为:%v\n",reflect.TypeOf(name),reflect.TypeOf(v))
+	fmt.Printf("自定义类型为:%v,接口类型为:%v\n", reflect.TypeOf(name), reflect.TypeOf(v))
 	v = name1
-	fmt.Printf("自定义类型为:%v,接口类型为:%v\n",reflect.TypeOf(name),reflect.TypeOf(v))
+	fmt.Printf("自定义类型为:%v,接口类型为:%v\n", reflect.TypeOf(name), reflect.TypeOf(v))
 	// 调用方法，把a、e、i、o、u在两个自定义类型变量的值所存在的字符的打印出来
 	fmt.Printf("Vowels are %c", v.FindVowels())
-
 }
