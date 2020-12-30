6ce048349723b606dee201a914a682923478cb40
Desist-Daydream <39847025+Desist-Daydream@users.noreply.github.com>
2020-12-30 23:21:19 +0800

    试接口多态效果，实现了接口的接口体，可以作为该接口的值。

--- 7.Structs_Interfaces/struct/struct.go
+++ 7.Structs_Interfaces/struct/struct.go
@@ -1,32 +1,34 @@
 package main
 
-import "fmt"
-
-func main() {
-    struct2()
-}
+import (
+	"fmt"
+)
 
 // 定义一个名为Struct1的结构体，里面有3个字段
-type struct1 struct {
-    i1  int
-    f1  float32
-    str string
+type structDemo struct {
+	i1  int
+	f1  float32
+	str string
 }
 
-func struct2() {
-	// 声明一个名为ms的变量，使用struct1类型,也可以使用new函数进行初始化
-	var ms struct1
-	// ms := new(struct1)
+func structDemoFunction() {
+	// 声明一个名为 sd 的变量，使用 structDemo 类型,也可以使用 new 函数进行初始化
+	var sd structDemo
+	// sd := new(structDemo)
 
 	// 对结构体中的每个字段进行赋值
-	ms.i1 = 10
-	ms.f1 = 15.5
-	ms.str = "Chris"
+	sd.i1 = 10
+	sd.f1 = 15.5
+	sd.str = "Chris"
 	// 也可以直接使用下面的初始化方式，对结构体进行初始化赋值，这种赋值方式是按照字段从上到下的顺序进行赋值
-	// ms := &struct1{10, 15.5, "Chris"}
+	// sd := &structDemo{10, 15.5, "Chris"}
 
-	fmt.Printf("The int is: %d\n", ms.i1)
-	fmt.Printf("The float is: %f\n", ms.f1)
-	fmt.Printf("The string is: %s\n", ms.str)
-	fmt.Println(ms)
+	fmt.Printf("The int is: %d\n", sd.i1)
+	fmt.Printf("The float is: %f\n", sd.f1)
+	fmt.Printf("The string is: %s\n", sd.str)
+	fmt.Println(sd)
+}
+
+func main() {
+	structDemoFunction()
 }
--- practice/usb_interface/main.go
+++ practice/usb_interface/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	usbdevice "github.com/DesistDaydream/GoLearning/practice/usb_interface/usb_device"
 	usbinterface "github.com/DesistDaydream/GoLearning/practice/usb_interface/usb_interface"
 )
@@ -10,4 +12,10 @@ func main() {
 	usbinterface.OperatorRead(k)
 	m := usbdevice.NewMouse()
 	usbinterface.OperatorRead(m)
+
+	// usbs 测试接口多态效果，实现了接口的接口体，可以作为该接口的值。
+	var usbs = map[usbinterface.USB]bool{
+		&usbdevice.KingstonDisk{}: true,
+	}
+	fmt.Println(usbs)
 }
