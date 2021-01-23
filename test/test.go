0f98743b4492b7f9a716505573f2852cff159f97
Desist-Daydream <39847025+Desist-Daydream@users.noreply.github.com>
2021-01-23 20:41:32 +0800

    2021.1.23

--- test/test.go
+++ test/test.go
@@ -2,43 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	uuid "github.com/satori/go.uuid"
 )
 
-// KingstonDisk 金士顿牌移动硬盘
-type KingstonDisk struct {
-	Name string
-	Type string
-	Data string
-}
-
-// NewKingstonDisk is
-func NewKingstonDisk() *KingstonDisk {
-	return &KingstonDisk{
-		Name: "A1",
-		Type: "SSD",
-		Data: "KingstonDisk fastest SSD",
-	}
-}
-
-// Start is
-func (k *KingstonDisk) Start() {
-	fmt.Println("金士顿SSD硬盘已连接")
-}
-
-// End is
-func (k *KingstonDisk) End() {
-	//
-}
-
-// OperatorRead is
-func OperatorRead(u *KingstonDisk) {
-	u.Start()
-	fmt.Printf("当前连接的设备信息为：%v\n", u)
-	u.End()
-}
-
 func main() {
-	//
-	k := NewKingstonDisk()
-	OperatorRead(k)
+	u1 := uuid.Must(uuid.NewV4(), nil)
+	fmt.Printf("UUIDv4: %s\n", u1)
 }
