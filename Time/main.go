7379ba1a71d8a1c41fb1703155608f6b54e9ef25
DesistDaydream <373406000@qq.com>
2021-04-21 14:42:44 +0800

    Update main.go

--- Time/main.go
+++ Time/main.go
@@ -48,7 +48,15 @@ func ticker() {
 	}
 }
 
+func timestamp() {
+	fmt.Printf("时间戳（秒）：%v;\n", time.Now().Unix())
+	fmt.Printf("时间戳（纳秒）：%v;\n", time.Now().UnixNano())
+	fmt.Printf("时间戳（毫秒）：%v;\n", time.Now().UnixNano()/1e6)
+	fmt.Printf("时间戳（纳秒转换为秒）：%v;\n", time.Now().UnixNano()/1e9)
+}
+
 func main() {
 	// timer()
-	ticker()
+	// ticker()
+	timestamp()
 }
