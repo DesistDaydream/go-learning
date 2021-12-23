cd72f20d9ac1674f4a207bba9c34e4c94e2f4dff
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2021-12-23 23:41:00 +0800

    Update pointer.go

--- 6.Pointers/pointer.go
+++ 6.Pointers/pointer.go
@@ -12,8 +12,8 @@ var (
 func BasePointer() {
 	a = 5
 	// 输出a的值与a的地址值
-	fmt.Printf("变量a的值为:%v\t变量a的指针所指向的地址值为：%v\n",a, &a)
-	
+	fmt.Printf("变量a的值为:%v\t变量a的指针所指向的地址值为：%v\n", a, &a)
+
 	// 指针必须要有一个合法指向的变量的地址才可以操作，否则会报错
 	// 定义一个指针类型的变量p，并赋值为空，但是会报错。因为指针p没有指向任何的地址
 	// var p *int	//单独定义指针，值为nil，会报错
@@ -29,16 +29,30 @@ func BasePointer() {
 	// 把变量a的地址值给p，两者的地址值是不一样的
 	// 所以指针指向的是变量地址，且指针的指针还是一个指针
 	p = &a
-	fmt.Printf("变量p的类型为：%v\n",reflect.TypeOf(p))
-	fmt.Printf("变量p的值为：%v\n变量a的值为%v\n变量p的指针地址为：%v\n变量a的指针地址为：%v\n",p, a, &p, &a)
+	fmt.Printf("变量p的类型为：%v\n", reflect.TypeOf(p))
+	fmt.Printf("变量p的值为：%v\n变量a的值为%v\n变量p的指针地址为：%v\n变量a的指针地址为：%v\n", p, a, &p, &a)
 }
 
-func doSomething(i *int, num int) {
-	*i = num
-	fmt.Println(i, num, *i)
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
 }
 
 func main() {
 	BasePointer()
-	// doSomething(&a, 10)
+
+	// Go 通过指针，允许程序通过`引用传递`来传递值和数据结构
+	i := 1
+	fmt.Println("initial:", i)
+
+	// 通过值传递，在函数内修改不会影响原始值
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	// 通过引用传递，在函数内修改会影响原始值
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
 }
