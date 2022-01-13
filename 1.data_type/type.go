c3baa690930c6695a383c7aa8571a95187aebbb0
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-01-13 20:07:58 +0800

    数据类型转换练习

--- 1.data_type/type.go
+++ 1.data_type/type.go
@@ -5,25 +5,26 @@ import (
 	"reflect"
 )
 
+// 基本数据类型
+var intType = 12345
+var stringType = "string"
+var booleanType = true
+
+// 复合数据类型
+var sliceType []string
+var mapType = make(map[string]string)
+
+var structType struct{}
+var interfaceType interface{}
+
+var channelType = make(chan string)
+
+var pointType = new(int)
+
 func funcType() {}
 
-func main() {
-	// 基本数据类型
-	intType := 12345
-	stringType := "string"
-	booleanType := true
-
-	// 复合数据类型
-	var sliceType []string
-	mapType := make(map[string]string)
-
-	var structType struct{}
-	var interfaceType interface{}
-
-	channelType := make(chan string)
-
-	pointType := new(int)
-
+// 数据类型检查
+func dataType() {
 	// 基础数据类型检测
 	fmt.Println("检测数值变量的数据类型：", reflect.TypeOf(intType))
 	fmt.Println("检测字符串变量的数据类型：", reflect.TypeOf(stringType))
@@ -38,4 +39,24 @@ func main() {
 	fmt.Println("检测接口变量的数据类型：", reflect.TypeOf(interfaceType))
 	fmt.Println("检测通道变量的数据类型：", reflect.TypeOf(channelType))
 	fmt.Println("检测指针变量的数据类型：", reflect.TypeOf(pointType))
+}
+
+// 数据类型转换
+func typeTransformation() {
+	// string to []byte
+	s1 := "hello"
+	b := []byte(s1)
+
+	// []byte to string
+	s2 := string(b)
+
+	fmt.Println(s2)
+}
+
+func main() {
+	// 数据类型检查
+	dataType()
+
+	// 数据类型转换
+	typeTransformation()
 }
