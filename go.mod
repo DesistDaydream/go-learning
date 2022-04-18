be091e03817b638b626c0cd60d752798939bfd51
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-04-18 22:42:32 +0800

    完善泛型基础

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/DesistDaydream/go-learning
 
 go 1.18
 
-require (
-	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd
-	gopkg.in/yaml.v2 v2.4.0
-)
+require gopkg.in/yaml.v2 v2.4.0
--- pkg/11_generic/generic.go
+++ pkg/11_generic/generic.go
@@ -1,20 +1,28 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"golang.org/x/exp/constraints"
-)
-
-func GMin[T constraints.Ordered](x, y T) T {
-	if x < y {
-		return x
+// 泛型
+// 使用类型形参编写 Go 函数以处理多种类型
+// comparable 是一个内置 Constraint(约束)，用来表示类型形参可以接收的实参的种类，所谓的“约束”就是指，T 被约束为可以使用哪几种类型。
+// comparable 包含所有可以比较类型，包括：booleans、numbers、strings、pointers、channels、可比较的 arrays、structs 中的属性
+// comparable 可以改为 any，表示 T 可以是任意类型
+func Index[T comparable](s []T, x T) int {
+	for i, v := range s {
+		// 这里的 v 和 x 都是 T 类型，且 T 类型具有可以比较的约束，因此这里可以使用 ==
+		if v == x {
+			return i
+		}
 	}
-	return y
+	return -1
 }
 
 func main() {
-	fmt.Println(GMin(1, 2))
-	fmt.Println(GMin("a", "b"))
-	fmt.Println(GMin(3.14, 3.141592653589793))
+	// Index() 函数适用于 int 类型的切片
+	si := []int{10, 20, 15, -10}
+	fmt.Println(Index(si, 15))
+
+	//  Index() 函数同样也使用于 string 类型的切片
+	ss := []string{"foo", "bar", "baz"}
+	fmt.Println(Index(ss, "hello"))
 }
