327f2d91d725c59036a118b0540b672b663590a6
DesistDaydream <373406000@qq.com>
2022-05-26 17:30:55 +0800

    Create context.go

---
+++ pkg/10_goroutine_and_channel/concurrency_control/context/context.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+const shortDuration = 10 * time.Second
+
+func Control() []time.Time {
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), shortDuration)
+	defer cancel()
+
+	var t []time.Time
+
+	for {
+		select {
+		// 在此循环期间，如果没有超时，则持续执行
+		// 这个地方通常是一个阻塞的操作，由通过 Channel 持续从其他地方获取数据
+		// 直到 ctx.Done 时，将从 Channel 中获取到的数据 return 出去
+		case TimeData := <-time.After(3 * time.Second):
+			fmt.Println(TimeData)
+			t = append(t, TimeData)
+		// 如果超时，则直接返回
+		case <-ctx.Done():
+			return t
+		}
+	}
+}
+
+func main() {
+	fmt.Println(Control())
+}
