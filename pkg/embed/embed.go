749bbed5342f719210e15da4f04b7b500feae30a
DesistDaydream <373406000@qq.com>
2022-06-08 15:31:25 +0800

    添加 embed 库

--- pkg/embed/embed.go
+++ pkg/embed/embed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"fmt"
+	"net/http"
 )
 
 var (
@@ -14,9 +15,21 @@ var (
 	f embed.FS
 )
 
+func httpEmbed() {
+	http.Handle("/", http.FileServer(http.FS(f)))
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
+	// 基础使用
+	// 将嵌入的文件转为字符串
 	fmt.Println(s)
+	// 将嵌入的文件转为字节数组
 	fmt.Println(string(b))
+	// 将嵌入的文件转为文件系统
 	data, _ := f.ReadFile("hello.txt")
 	fmt.Println(string(data))
+
+	// 常用于 Web 服务，以嵌入 HTML 等静态文件
+	httpEmbed()
 }
