851db82553159329c1c87f2bcece1ece5d181d7e
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-08-16 22:59:13 +0800

    8.16

--- pkg/text_handle/regular/regular.go
+++ pkg/text_handle/regular/regular.go
@@ -6,16 +6,19 @@ import (
 	"regexp"
 )
 
+// 匹配中文
+var exprCN = "[\u4e00-\u9fa5]"
+
 func main() {
 	srcFile := "test_files/test.txt"
 	// 待处理文本
 	fileByte, _ := os.ReadFile(srcFile)
 
 	// 编写正则表达式
-	expr := `^(\s*)\n` // 匹配空行，这个表达式有问题，待验证，暂时搜不到 Go 中如何删除空白行的方式
+	var exprLine = `\s*\n` // 匹配空白字符任意数后接换行符，暂时搜不到 Go 中如何直接匹配空行
 	// 使用正则表达式实例化一个 Regexp 对象。这里称之为编译正则表达式，也可以称之为创建正则表达式对象。
-	regexp := regexp.MustCompile(expr)
-	// 使用正则表达式对象的 ReplaceAllString 方法，将匹配到的字符串替换为空
-	result := regexp.ReplaceAllString(string(fileByte), "")
+	regexp := regexp.MustCompile(exprLine)
+	// 使用正则表达式对象的 ReplaceAllString 方法，将匹配到的字符串替换为换行符
+	result := regexp.ReplaceAllString(string(fileByte), "\n")
 	fmt.Println(result)
 }
