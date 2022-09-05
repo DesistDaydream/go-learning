5773e913dfe5abb238067ae819eb2f3be5c43e57
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-09-05 23:39:54 +0800

    Update pointer.go

--- pkg/7_pointer/pointer.go
+++ pkg/7_pointer/pointer.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
 var (
 	a int
 )
 
-func main() {
-	a = 5
+func BasePointer() {
 	// 输出a的值与a的地址值
 	fmt.Printf("变量a的值为:%v\t变量a的指针所指向的地址值为：%v\n", a, &a)
 
@@ -31,4 +31,24 @@ func main() {
 	p = &a
 	fmt.Printf("变量p的类型为：%v\n", reflect.TypeOf(p))
 	fmt.Printf("变量p的值为：%v\n变量a的值为%v\n变量p的指针地址为：%v\n变量a的指针地址为：%v\n", p, a, &p, &a)
+}
+
+func main() {
+	normalVar := 5
+	// 通过 & 符号取得变量 a 的内存地址，即指向 a 的指针
+	fmt.Println("变量 a 的内存地址，即指针为：", &normalVar)
+
+	// 声明一个指针类型的变量
+	// TODO: 使用 var 和 new() 有什么区别？ 使用 var 的话，没法 *ptr="a" 这么用，但是用 new() 就可以
+	var ptr *string // 这是一个字符串指针类型的变量
+	// ptr := new(string)
+	fmt.Println("刚刚声明的指针没有任何内存地址，默认值为 nil：", ptr)
+
+	// 指针赋值
+	// 错误示例：不可以使用 *int 类型给 *string 类型赋值，虽然都是内存地址，但是不可以这么做
+	// ptr = &normalVar
+	// 正确示例:
+	newPtr := strconv.Itoa(normalVar)
+	ptr = &newPtr
+	fmt.Println("为指针类型变量赋予一个内存地址后，获取该内存地址内保存的值：", *ptr)
 }
