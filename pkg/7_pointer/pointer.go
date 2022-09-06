99e8b9ec32770e332d17822a288392c0ee2f935a
DesistDaydream <373406000@qq.com>
2022-09-06 10:06:39 +0800

    Update pointer.go

--- pkg/7_pointer/pointer.go
+++ pkg/7_pointer/pointer.go
@@ -2,56 +2,44 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"strconv"
 )
 
-var (
-	a int
-)
-
 func BasePointer() {
-	// 输出a的值与a的地址值
-	fmt.Printf("变量a的值为:%v\t变量a的指针所指向的地址值为：%v\n", a, &a)
-
-	// 指针必须要有一个合法指向的变量的地址才可以操作，否则会报错
-	// 定义一个指针类型的变量p，并赋值为空，但是会报错。因为指针p没有指向任何的地址
-	// var p *int	//单独定义指针，值为nil，会报错
-	// p = &a	//在p有合法指向的时候，p即可使用
-	// 使用new函数来定义一个指针，会为该指针分配一个没有标识符的变量的地址
-	// 这种用法叫做动态分配空间
-	p := new(int)
-	fmt.Println("指针变量p所指向的地址值为：", p, "该指针p所指向的无标识符变量的值为:", *p)
-	*p = 6
-	a = *p
-	fmt.Println(a, *p, &a, p)
-
-	// 把变量a的地址值给p，两者的地址值是不一样的
-	// 所以指针指向的是变量地址，且指针的指针还是一个指针
-	p = &a
-	fmt.Printf("变量p的类型为：%v\n", reflect.TypeOf(p))
-	fmt.Printf("变量p的值为：%v\n变量a的值为%v\n变量p的指针地址为：%v\n变量a的指针地址为：%v\n", p, a, &p, &a)
-}
-
-func main() {
 	normalVar := 5
 	// 通过 & 符号取得变量 a 的内存地址，即指向 a 的指针
 	fmt.Println("变量 a 的内存地址，即指针为：", &normalVar)
 
 	// 声明一个指针类型(字符串指针类型)的变量，两种方式：
 	var ptrVar *string
-	fmt.Println("指针的声明方式一。通过 var 关键字声明的指针，没有任何内存地址，默认值为 nil：", ptrVar)
-	ptrNew := new(string)
-	fmt.Println("指针的声明方式二。通过 new() 函数声明的指针，系统会为该变量分配内存地址，而不是 nil：", ptrNew)
+	fmt.Println("通过 var 关键字声明的指针变量的值，默认为 nil：", ptrVar)
+	fmt.Println("但是这个指针变量本身是存在内存地址的：", &ptrVar)
+	// 由于此时 ptr 为 nil，并不具有内存地址，所以不能通过解引用的方式为指针所指向的内存地址中添加值。毕竟现在还没有内存地址了
+	// *ptrVar = "pointer string"
 
-	// 指针赋值
-	// 错误示例：不可以使用 *int 类型给 *string 类型赋值，虽然都是内存地址，但是不可以这么做
+	// 指针变量的赋值。
+	// 给指针类型的变量赋值，赋予的是内存地址，不是字符串、整数之类的。内存地址类似于这样：0xc000014088
+	newPtrVar := strconv.Itoa(normalVar)
+	ptrVar = &newPtrVar
+	fmt.Println("为指针变量赋予一个内存地址后，即可以通过该变量获取到内存地址中保存的值：", *ptrVar)
+	// 错误示例：不可以使用 *int 类型给 *string 类型赋值，虽然指针变量的值都是内存地址，但是不可以这么做
 	// ptr = &normalVar
-	// 正确示例:
-	newPtr := strconv.Itoa(normalVar)
-	ptrVar = &newPtr // 由于此时 ptr 为 nil，并不具有内存地址，所以不能通过解引用的方式赋值
-	fmt.Println("指针的赋值方式一。为指针类型变量赋予一个内存地址后，获取该内存地址内保存的值：", *ptrVar)
+}
 
-	*ptrNew = newPtr // 由于此时 ptrNew 具有内存地址，所以可以直接通过解引用的方式为 ptrNew 内存地址空间内赋予值
-	fmt.Println("指针的赋值方式二。通过解引用的方式，直接将值放到指针所指向的内存地址内部：", *ptrNew)
+func Instantiation() {
+	// 我们可以使用 new() 函数直接实例化一个 *string 类型的变量，此时系统会自动为指针变量赋予一个值。
+	// 也就是说，实例化一个指针类型的变量，系统会开辟出两块内存空间
+	// 1. 第一块内存空间存储指针变量本身的值
+	// 2. 第二块内存空间就是变量的值所指向的内存空间，用以存放我们想要的值
+	ptrVar := new(string)
+	fmt.Println("通过 new() 函数实例化指针，系统会为该变量的值赋予一个内存地址：", ptrVar)
+
+	// 由于此时 ptrNew 具有内存地址，所以可以直接通过解引用的方式为 ptrNew 内存地址空间内赋予值
+	*ptrVar = "pointer string"
+	fmt.Println("通过解引用的方式，直接将值放到指针变量的值所指向的内存地址内部：", *ptrVar)
+}
+
+func main() {
+	// BasePointer()
+	Instantiation()
 }
