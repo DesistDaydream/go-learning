a57686a3736ecbe7c63697b568b5f8c47aa348e1
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-09-19 15:38:32 +0800

    2022.9.19

--- pkg/8_struct/struct.go
+++ pkg/8_struct/struct.go
@@ -8,9 +8,9 @@ import (
 // 每个字段都可以是任意类型的数据。
 // **结构体的定义**。定义一个名为 structDemo 的结构体，里面有3个字段
 type structDemo struct {
-	i1  int
-	f1  float32
-	str string
+	fieldOneForInt      int
+	fieldTwoForFloat32  float32
+	fieldThreeForString string
 }
 
 func newStructDemo() *structDemo {
@@ -28,16 +28,18 @@ func structDemoFunction() {
 	// 当然，最常见的是自己定义一个函数用以实例化结构体，类似于自己实现一个功能更全面的 new 函数
 	// sd := newStructDemo()
 
+	// **结构体的引用与赋值**
+	// 使用 . 符号引用结构体中的字段，这个符号称为选择器
 	// 对结构体中的每个字段进行赋值
-	sd.i1 = 10
-	sd.f1 = 15.5
-	sd.str = "Chris"
+	sd.fieldOneForInt = 10
+	sd.fieldTwoForFloat32 = 15.5
+	sd.fieldThreeForString = "Chris"
 	// 也可以直接使用下面的初始化方式，对结构体进行初始化赋值，这种赋值方式是按照字段从上到下的顺序进行赋值
 	// sd := &structDemo{10, 15.5, "Chris"}
 
-	fmt.Printf("The int is: %d\n", sd.i1)
-	fmt.Printf("The float is: %f\n", sd.f1)
-	fmt.Printf("The string is: %s\n", sd.str)
+	fmt.Printf("The int is: %d\n", sd.fieldOneForInt)
+	fmt.Printf("The float is: %f\n", sd.fieldTwoForFloat32)
+	fmt.Printf("The string is: %s\n", sd.fieldThreeForString)
 	fmt.Println(sd)
 }
 
