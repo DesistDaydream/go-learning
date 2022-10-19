ca20dba2f503b9071dc8eb588757de3e32fb989b
DesistDaydream <373406000@qq.com>
2022-10-19 15:12:03 +0800

    时间格式化

--- pkg/time/main.go
+++ pkg/time/main.go
@@ -55,8 +55,32 @@ func timestamp() {
 	fmt.Printf("时间戳（纳秒转换为秒）：%v;\n", time.Now().UnixNano()/1e9)
 }
 
+func timeFormat() {
+	currentTime := time.Now()
+	fmt.Println("当前时间  : ", currentTime)
+	fmt.Println("当前时间字符串: ", currentTime.String())
+	fmt.Println("MM-DD-YYYY : ", currentTime.Format("01-02-2006"))
+	fmt.Println("YYYY-MM-DD : ", currentTime.Format("2006-01-02"))
+	fmt.Println("YYYY.MM.DD : ", currentTime.Format("2006.01.02 15:04:05"))
+	fmt.Println("YYYY#MM#DD {Special Character} : ", currentTime.Format("2006#01#02"))
+	fmt.Println("YYYY-MM-DD hh:mm:ss : ", currentTime.Format("2006-01-02 15:04:05"))
+	fmt.Println("Time with MicroSeconds: ", currentTime.Format("2006-01-02 15:04:05.000000"))
+	fmt.Println("Time with NanoSeconds: ", currentTime.Format("2006-01-02 15:04:05.000000000"))
+	fmt.Println("ShortNum Month : ", currentTime.Format("2006-1-02"))
+	fmt.Println("LongMonth : ", currentTime.Format("2006-January-02"))
+	fmt.Println("ShortMonth : ", currentTime.Format("2006-Jan-02"))
+	fmt.Println("ShortYear : ", currentTime.Format("06-Jan-02"))
+	fmt.Println("LongWeekDay : ", currentTime.Format("2006-01-02 15:04:05 Monday"))
+	fmt.Println("ShortWeek Day : ", currentTime.Format("2006-01-02 Mon"))
+	fmt.Println("ShortDay : ", currentTime.Format("Mon 2006-01-2"))
+	fmt.Println("Short Hour Minute Second: ", currentTime.Format("2006-01-02 3:4:5"))
+	fmt.Println("Short Hour Minute Second: ", currentTime.Format("2006-01-02 3:4:5 PM"))
+	fmt.Println("Short Hour Minute Second: ", currentTime.Format("2006-01-02 3:4:5 pm"))
+}
+
 func main() {
 	// timer()
 	// ticker()
-	timestamp()
+	// timestamp()
+	timeFormat()
 }
