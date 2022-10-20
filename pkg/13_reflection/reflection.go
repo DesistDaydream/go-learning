3ccd150cfc03071711d4cc1efaff777ed23dc12c
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-10-20 22:23:13 +0800

    完善反射功能示例

--- pkg/13_reflection/reflection.go
+++ pkg/13_reflection/reflection.go
@@ -6,7 +6,7 @@ import (
 )
 
 type NeedReflectStruct struct {
-	Name string `json:"name"`
+	Name string `json:"name" yaml:"name"`
 	Age  int    `json:"age"`
 }
 
@@ -31,25 +31,46 @@ func ReflectAndStruct() {
 	// NumField() 方法只适用于 Struct 类型，返回结构体类型中所有字段的总数。
 	for i := 0; i < t.NumField(); i++ {
 		// Field() 方法只适用于 Struct 类型，返回结构体各个字段的信息
-		fieldInfo := t.Field(i)
+		fieldInstance := t.Field(i)      // 对于字段来说，Field() 返回一个关于结构体字段的实例
+		fieldValueInstance := v.Field(i) // 对于值来说，Field() 返回一个关于结构体字段的值的实例
+
+		fmt.Printf("字段实例: %v\n", fieldInstance)
+		fmt.Printf("字段值实例: %v\n", fieldValueInstance)
+
 		// 字段信息包括该字段的 名称、包路径、数据类型、标签 等等
-		fmt.Printf("字段名称：%v\n", fieldInfo.Name)
-		fmt.Printf("字段包路径：%v\n", fieldInfo.PkgPath)
-		fmt.Printf("字段标签：%v\n", fieldInfo.Tag)
+		fmt.Printf("第 %v 个字段名称：%v\n", i+1, fieldInstance.Name)
+		fmt.Printf("第 %v 个字段标签：%v\n", i+1, fieldInstance.Tag)
+		// 获取结构体中 Tag 的值
+		fmt.Printf("第 %v 个字段标签中名为 json 的值: %v\n", i+1, fieldInstance.Tag.Get("json"))
+
+		// 通过 值实例 的 Interface() 方法获取字段的值
+		fmt.Printf("第 %v 个字段值：%v\n", i+1, fieldValueInstance.Interface())
 	}
+
+	// Kind() 方法用来获取目标的类型，常用来执行比较运算
+	if t.Kind() == reflect.Struct {
+		fmt.Println("这是一个 Struct 类型")
+	}
+
+	// SetXXX() 方法可以让我们修改变量的值
+	// 这里我们通过 FieldByName() 找到名为 Name 的字段，然后使用 SetString() 修改 Name 字段的值
+	v.FieldByName("Name").SetString("新名字")
+	fmt.Println(s)
 
 }
 
 func Reflect() {
-	var strVar string
-	// 获取一个变量的类型
-	t := reflect.TypeOf(strVar)
-	// 获取一个变量的值
-	v := reflect.ValueOf(strVar)
-	fmt.Println(t, v)
+	var intVar int
+	// 获取一个变量的类型。返回 Type{} 接口，该接口有很多方法用以获取该变量“类型”的相关信息。
+	t := reflect.TypeOf(intVar)
+	// 获取一个变量的值。返回 Value{} 结构体，该结构体也有很多方法用以获取该变量“值”的相关信息。
+	v := reflect.ValueOf(intVar)
+
+	fmt.Println("类型: ", t)
+	fmt.Println("值: ", v)
 }
 
 func main() {
-	// Reflect()
-	ReflectAndStruct()
+	Reflect()
+	// ReflectAndStruct()
 }
--- pkg/8_struct/reflect_and_struct_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestGetStructAllInfo(t *testing.T) {
-	GetStructAllInfo()
-}
