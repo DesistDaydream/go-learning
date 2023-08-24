bc7321c522bf4500a8f0922fe2e5a35647989346
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2023-08-24 17:30:13 +0800

    Update for_attention.go

--- pkg/3_control_structure/for/for_attention.go
+++ pkg/3_control_structure/for/for_attention.go
@@ -40,3 +40,66 @@ func forPointerCorrect() {
 	fmt.Println("Values:", *out[0], *out[1], *out[2])
 	fmt.Println("Addresses:", out[0], out[1], out[2])
 }
+
+type person struct {
+	name string
+	age  int
+}
+
+type company struct {
+	ps []cp
+}
+
+type cp struct {
+	person *person
+}
+
+// 使用循环迭代变量的指针
+// 错误的做法
+func forAttentionStructError() {
+	ps := []person{
+		{
+			name: "lch",
+			age:  30,
+		},
+		{
+			name: "zn",
+			age:  300,
+		},
+	}
+
+	var out company
+	for _, p := range ps {
+		println(&p)
+		fmt.Println(&p)
+		out.ps = append(out.ps, cp{
+			person: &p,
+		})
+	}
+
+	fmt.Println(ps)
+
+	for _, v := range out.ps {
+		fmt.Println(v.person)
+	}
+	fmt.Println("Values:", out.ps[0], out.ps[1])
+
+	// 注意：若是多层 for 循环，那内层的循环则由于其他原因导致每次迭代的值的指针是不同的，就像下面这段代码
+	// for _, card := range cards.Data {
+	// 	for _, p := range products.Data {
+	// 		println(&card)
+	// 		println(&p)
+	// 	}
+	// }
+	// 输出的结果是：
+	// 0xc0003a2d80
+	// 0xc00028a500
+	// 0xc0003a2d80
+	// 0xc000168140
+	// 0xc0003a2d80
+	// 0xc00028a640
+	// 0xc0003a2d80
+	// 0xc000168280
+	// 这是因为，for 循环中的变量地址是固定的，但是在嵌套循环中，内部循环的变量地址会在每次外部循环迭代时重新分配。
+	// 因此，在您提供的代码中，card 变量的地址在整个循环过程中保持不变，而 p 变量的地址则会在每次外部循环迭代时改变。
+}
