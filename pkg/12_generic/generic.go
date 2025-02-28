ee8c4faaf22b70d52fbe654337218952fdecf6da
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2025-02-28 17:25:55 +0800

    Update generic.go

--- pkg/12_generic/generic.go
+++ pkg/12_generic/generic.go
@@ -19,7 +19,21 @@ func Index[T comparable](s []T, x T) int {
 	return -1
 }
 
+type Number interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | string
+}
+
+func GenericDemo[T Number](s, x T) T {
+	return s + x
+}
+
 func main() {
+	// 泛型。是对类型的抽象（i.e. 同一系列的对象群体，这个对象是指类型，这一类类型的抽象）（并不是对代码逻辑的抽象）
+	// 在一个代码块中，若其中不同类型的处理逻辑一样，但是其中的类型必须是相同种类。这就是使用泛型的最佳时机之一
+	// 非人话描述：对类型的抽象的相同逻辑泛用化
+	fmt.Println(GenericDemo(1, 2))
+	fmt.Println(GenericDemo("desist", "daydaream"))
+
 	// Index() 函数适用于 int 类型的切片
 	// 可以像这样 `Index[int](si, 15)` 传递参数告诉 `Index()` 当前应该使用哪种类型类型执行其中的代码，这里的 `[int]` 就是告诉 Index 的 T 应该是 int 类型，
 	// 其调用相当于 `func Index[T comparable](s []T, x T) int {}` 变成了 `func Index(s []int, x int) int {}`
